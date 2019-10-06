3a162bccb2e04000fbc36671bde7a080d863a430
Katz, Matan <matan.katz@intel.com>
2019-10-06 10:55:36 +0300

    android - projector control ui

--- wrappers/android/tools/camera/src/main/java/com/intel/realsense/camera/PreviewActivity.java
+++ wrappers/android/tools/camera/src/main/java/com/intel/realsense/camera/PreviewActivity.java
@@ -13,15 +13,22 @@ import android.util.Pair;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.widget.RadioButton;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.intel.realsense.librealsense.Config;
+import com.intel.realsense.librealsense.Device;
+import com.intel.realsense.librealsense.DeviceList;
 import com.intel.realsense.librealsense.FrameSet;
 import com.intel.realsense.librealsense.GLRsSurfaceView;
+import com.intel.realsense.librealsense.Option;
+import com.intel.realsense.librealsense.RsContext;
+import com.intel.realsense.librealsense.Sensor;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class PreviewActivity extends AppCompatActivity {
@@ -33,6 +40,7 @@ public class PreviewActivity extends AppCompatActivity {
     private TextView mSettingsButton;
     private TextView mStatisticsButton;
     private TextView m3dButton;
+    private TextView mControlsButton;
 
     private TextView mStatsView;
     private Map<Integer, TextView> mLabels;
@@ -70,6 +78,7 @@ public class PreviewActivity extends AppCompatActivity {
         mSettingsButton = findViewById(R.id.preview_settings_button);
         mStatisticsButton = findViewById(R.id.preview_stats_button);
         m3dButton = findViewById(R.id.preview_3d_button);
+        mControlsButton = findViewById(R.id.controls_button);
 
         mStartRecordFab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -106,6 +115,14 @@ public class PreviewActivity extends AppCompatActivity {
                 SharedPreferences.Editor editor = sharedPref.edit();
                 editor.putBoolean(getString(R.string.show_3d), mShow3D);
                 editor.commit();
+            }
+        });
+        mControlsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ControlsDialog cd = new ControlsDialog();
+                cd.setCancelable(true);
+                cd.show(getFragmentManager(), null);
             }
         });
         mStatisticsButton.setOnClickListener(new View.OnClickListener() {
@@ -229,5 +246,50 @@ public class PreviewActivity extends AppCompatActivity {
             mStreamer.stop();
         if(mGLSurfaceView != null)
             mGLSurfaceView.clear();
+    }
+
+    public void onRadioButtonClicked(View view) {
+        boolean checked = ((RadioButton) view).isChecked();
+
+        if(!checked)
+            return;
+        RsContext ctx = new RsContext();
+        try(DeviceList devices = ctx.queryDevices()) {
+            try(Device device = devices.createDevice(0)){
+                if(device == null)
+                    return;
+                List<Sensor> sensors = device.querySensors();
+                for(Sensor s : sensors){
+                    if(!s.supports(Option.EMITTER_ENABLED))
+                        continue;
+                    switch(view.getId()) {
+                        case R.id.radio_no_projector:{
+                            if(s.supports(Option.EMITTER_ENABLED))
+                                s.setValue(Option.EMITTER_ENABLED, 0);
+                            if(s.supports(Option.LED_ENABLED))
+                                s.setValue(Option.LED_ENABLED, 0);
+                            break;
+                        }
+                        case R.id.radio_laser:{
+                            if(s.supports(Option.PROJECTOR_TYPE))
+                                s.setValue(Option.PROJECTOR_TYPE, 0);
+                            if(s.supports(Option.EMITTER_ENABLED))
+                                s.setValue(Option.EMITTER_ENABLED, 1);
+                            break;
+                        }
+                        case R.id.radio_led:{
+                            if(s.supports(Option.PROJECTOR_TYPE))
+                                s.setValue(Option.PROJECTOR_TYPE, 1);
+                            if(s.supports(Option.LED_ENABLED))
+                                s.setValue(Option.LED_ENABLED, 1);
+                            break;
+                        }
+                    }
+                }
+            } catch(Exception e){
+                Log.e(TAG, "Failed to set controls: " + e.getMessage());
+                Toast.makeText(this, "Failed to set controls", Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
